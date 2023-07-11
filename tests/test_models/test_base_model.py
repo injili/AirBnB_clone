3fd96927628836e6da10b02ff2e8ee50de2920dc
injili <nyarekigospel@gmail.com>
2023-07-11 14:26:45 -0700

    init fahm

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 """
-Tests for the base_model module
+unittests for the base_model module
+#run with either of the following commands:
+    python3 -m unittest discover tests
+    python3 -m unittest tests/test_models/test_base_model.py
 """
+
+
+class TestBaseModel(unittest.TestCase):

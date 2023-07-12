dc21f551e0869545aa0c0816e162af433caa95a5
injili <nyarekigospel@gmail.com>
2023-07-12 07:36:50 -0700

    unittest for the BaseModel class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,16 @@ unittests for the base_model module
     python3 -m unittest discover tests
     python3 -m unittest tests/test_models/test_base_model.py
 """
-
+import unittest
+BaseModel = base_model.BaseModel
 
 class TestBaseModel(unittest.TestCase):
+    """
+    All the tests on the class BaseModel
+    """
+
+    def test_class(self):
+        """
+        test the functionality of the class
+        """
+        self.assertTrue(type(BaseModel()), BaseModel)

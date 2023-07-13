2c7ce84f4cdd02c539a89c136737cd3af38f7988
injili <nyarekigospel@gmail.com>
2023-07-13 13:25:32 -0700

    tests to the class instance attributes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ unittests for the base_model module
 """
 import unittest
 from datetime import datetime
+from uuid import UUID
 BaseModel = base_model.BaseModel
 
 class TestBaseModel(unittest.TestCase):
@@ -24,13 +25,21 @@ class TestBaseModel(unittest.TestCase):
         """
         test the public instance attributes
         """
-        r1.BaseModel()
+        r1 = BaseModel()
         ri.name = "Skito"
         r1.my_number = 99
         self.assertEqual(r1.name == "Skito")
         self.assertEqual(r1.my_number == 99)
+        self.assertIsInstance(r1.id, UUID)
+        self.assertNotEqual(r1.uuid, uuid4())
         self.assertIsInstance(r1.created_at, datetime)
         self.assertLessEqual(r1.created_at, datetime.now())
         self.assertGreaterEqual(r1.created_at, datetime(2020, 1, 1))
         self.assertIsInstance(r1.updated_at, datetime)
         self.assertGreaterEqual(r1.updated_at, datetime(2020, 1, 1))
+
+    def test_str_methods(self):
+        """
+        test the workings of the methods
+        """
+        r2 = BaseModel()

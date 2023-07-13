a4e5fa539d0ffdb500a6e52e09fd4cd7cdb25e3e
injili <nyarekigospel@gmail.com>
2023-07-13 13:07:01 -0700

    tests to the class instance attributes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@ unittests for the base_model module
     python3 -m unittest tests/test_models/test_base_model.py
 """
 import unittest
+from datetime import datetime
 BaseModel = base_model.BaseModel
 
 class TestBaseModel(unittest.TestCase):
@@ -21,10 +22,15 @@ class TestBaseModel(unittest.TestCase):
 
     def test_attributes(self):
         """
-        test the class attributes
+        test the public instance attributes
         """
         r1.BaseModel()
         ri.name = "Skito"
         r1.my_number = 99
         self.assertEqual(r1.name == "Skito")
         self.assertEqual(r1.my_number == 99)
+        self.assertIsInstance(r1.created_at, datetime)
+        self.assertLessEqual(r1.created_at, datetime.now())
+        self.assertGreaterEqual(r1.created_at, datetime(2020, 1, 1))
+        self.assertIsInstance(r1.updated_at, datetime)
+        self.assertGreaterEqual(r1.updated_at, datetime(2020, 1, 1))

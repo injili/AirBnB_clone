78e0e0a6c1fc66260e4483d723000ed6b0d1b2aa
injili <nyarekigospel@gmail.com>
2023-07-18 17:43:22 -0700

    the amenity model

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+amenity module
+"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    the amenity model implementation
+    """
+    name = ""

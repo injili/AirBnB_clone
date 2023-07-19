1c70b3c08933a361fd4da6ea6babed9a31948e99
injili <nyarekigospel@gmail.com>
2023-07-18 18:50:30 -0700

    console thing

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+the command interpreter that is the entry point
+"""
+
+import cmd
+from models.base_model import BaseModel
+from models import storage
+import re
+from shlex import split
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+def parse(arg):
+    """
+    content search
+    """
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if backets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl

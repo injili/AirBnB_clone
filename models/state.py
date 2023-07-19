c0e45833c6cac10d3056312919dbf3c42ae46fb9
injili <nyarekigospel@gmail.com>
2023-07-18 18:14:44 -0700

    the state module

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module state
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    the implementation of the state module
+    """
+    name = ""

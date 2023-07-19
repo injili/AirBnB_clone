522ee025242a54930c48ecd8886391c9bb93cfda
injili <nyarekigospel@gmail.com>
2023-07-18 17:26:36 -0700

    init file

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@
 modified __init__ to create a unique FileStorage instance
 """
 
-from models.base_model import BaseModel
 from  models.engine import file_storage
 
 storage = file_storage.FileStorage()

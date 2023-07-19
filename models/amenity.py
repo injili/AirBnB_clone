f7bbb1bfd802bf8158a3935fbc55b8b6610a65e1
injili <nyarekigospel@gmail.com>
2023-07-18 17:47:37 -0700

    the module modifications

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ amenity module
 
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
     """
     the amenity model implementation
---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module city
+"""
+
+
+class City(BaseModel):
+    """
+    the city class implementation
+    """
+    state_id = ""
+    name = ""
